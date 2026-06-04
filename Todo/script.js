c9f852297ee0560271367a938e3c9b0827bc805d
Sushant <unknownsushant.0042@gmail.com>
2026-06-04 16:09:53 +0530

    input rendering and displaying on screen

--- Todo/script.js
+++ Todo/script.js
@@ -3,3 +3,34 @@ const addButton = document.querySelector("#add-button");
 const todoList = document.querySelector("#todo-list");
 
 
+console.log(input);
+
+let tasks = [];
+
+addButton.addEventListener("click", addTask);
+
+function addTask(){
+    const taskText = input.value.trim();
+
+    if( taskText === ""){
+        return;
+    }
+    tasks.push({
+        text: taskText,
+        completed:false
+    });
+
+    renderTasks();
+    input.value = "";
+}
+
+function renderTasks(){
+    todoList.innerHTML="";
+
+    tasks.forEach(function(task){
+        const li = document.createElement("li");
+        li.textContent = task.text;
+        todoList.appendChild(li)
+    });
+
+}
